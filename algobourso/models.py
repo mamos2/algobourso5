610361acf7de48f0ff7767aec2377f891d060560
mamos2 <bassrimk35@gmail.com>
2017-12-22 15:57:36 +0100

    app algobourso

--- algobourso/models.py
+++ algobourso/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class User_profil (models.Model):
+	GENDER_CHOICES = (
+		('MAL', 'Homme'),
+		('FEM', 'Femme'),
+
+	)
+
+	user = models.OneToOneField(User, blank=True, null=True, on_delete=models.CASCADE)
+	adress = models.CharField(max_length=200)
+	gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
+
+
+	def __str__(self):
+		return self.user.username
+
+
